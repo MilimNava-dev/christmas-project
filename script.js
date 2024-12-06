1982a35ff55435b9150a3aa49cd56741860b5701
Milim Nava <milim.nava22194@gmail.com>
2024-12-06 19:24:20 +0100

    Add day checker, block days and today animation

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 const boxesGrid = document.querySelector('.boxes-table');
+const d = new Date();
+const day = d.getDate();
+const month = d.getMonth() + 1;
+
 function createBoxes () {
+    const daysNotAvalible = today()
     let html = ``
     for (let i = 0; i < 24; i++) {
-        html += `
-        <div class="div-box">
-            Day ${i + 1}
-        </div>`
+        if (daysNotAvalible.includes(i)) {
+            html += `
+            <div class="div-box not-avalible">
+                ${i + 1}
+            </div>`
+            continue
+        } else if (i === day - 1) {
+            html += `
+            <div class="div-box today">
+                ${i + 1}
+            </div>`
+        } else {
+            html += `
+            <div class="div-box">
+                ${i + 1}
+            </div>`
+        }
     }
     boxesGrid.innerHTML = html
 }
 createBoxes()
+
+function today() {
+    let daysNotAvalible = []
+    if (month === 12) {
+        for (let i = day; i < 24; i++) {
+            daysNotAvalible.push(i)
+        }
+    }
+    return daysNotAvalible
+}
