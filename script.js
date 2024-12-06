fcda30349a7e5d5e08afc7b5d6403873d2380e39
Milim Nava <milim.nava22194@gmail.com>
2024-12-06 20:04:16 +0100

    Add today's present + remove hover effects when viewing the gift

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const boxesGrid = document.querySelector('.boxes-table');
+const present = document.querySelector('.present-div')
 const d = new Date();
 const day = d.getDate();
 const month = d.getMonth() + 1;
@@ -9,18 +10,18 @@ function createBoxes () {
     for (let i = 0; i < 24; i++) {
         if (daysNotAvalible.includes(i)) {
             html += `
-            <div class="div-box not-avalible">
+            <div class="div-box avalible-hover not-avalible">
                 ${i + 1}
             </div>`
             continue
         } else if (i === day - 1) {
             html += `
-            <div class="div-box today">
+            <div class="div-box avalible-hover today">
                 ${i + 1}
             </div>`
         } else {
             html += `
-            <div class="div-box">
+            <div class="div-box avalible-hover">
                 ${i + 1}
             </div>`
         }
@@ -38,3 +39,22 @@ function today() {
     }
     return daysNotAvalible
 }
+
+const todayBox = document.querySelector('.today')
+const allBoxes = document.querySelectorAll('.div-box')
+todayBox.addEventListener('click', () => {
+    allBoxes.forEach(box => {
+        box.classList.remove('avalible-hover')
+    })
+    present.classList.add('visible')
+    present.addEventListener('animationend', () => {
+        const removeVisibleTimer = setTimeout(() => {
+            present.classList.remove('visible')
+            allBoxes.forEach(box => {
+                box.classList.add('avalible-hover')
+            })
+            clearTimeout(removeVisibleTimer)
+        },1000)
+    })
+})
+    
